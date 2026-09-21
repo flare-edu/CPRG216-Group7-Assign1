9b812b67c2d9f38258ff9fdab7588a569b02bf48
Ben <ben.hunt@edu.sait.ca>
2026-09-21 11:19:53 -0600

    initial selection and layout the code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # code go here
+print("--------------------------------------")
+print("*** Welcome to gas station program ***")
+print("--------------------------------------")
+print("Please select the type of purchase:")
+print("G: Gas")
+print("O: Oil")
+selection = input(">>>").upper()
+
+#DEFINE CONSTANTS HERE vvvvvvv
+
+#^^^^^^^^^
+
+if selection == "O":
+    # Oil option
+
+    pass #TODO: remove me when code is added
+elif selection == "G":
+    # Gas option
+
+    pass #TODO: remove me when code is added
+else:
+    print("Invalid input, you should enter g/G or o/O")
+    exit()
+
+#taxes and output
